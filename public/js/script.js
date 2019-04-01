4757bc6a8e897a6925e2a71b49f014f0cf6bb0d9
Maanav Dalal <maanavdalal@gmail.com>
2019-04-01 16:08:37 -0400

    Using wow.js for better animation performance.

--- public/js/script.js
+++ public/js/script.js
@@ -1,75 +1,75 @@
 /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
-particlesJS.load('particles-js', 'particles.json', function() {
-    console.log('callback - particles.js config loaded');
-  });
+particlesJS.load('particles-js', 'particles.json', function () {
+  console.log('callback - particles.js config loaded');
+});
 /* Bulma load navburger */
-$(document).ready(function() {
+$(document).ready(function () {
 
   // Check for click events on the navbar burger icon
-  $(".navbar-burger").click(function() {
+  $(".navbar-burger").click(function () {
 
-      // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-      $(".navbar-burger").toggleClass("is-active");
-      $(".navbar-menu").toggleClass("is-active");
+    // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+    $(".navbar-burger").toggleClass("is-active");
+    $(".navbar-menu").toggleClass("is-active");
 
   });
 });
 
-/* Loading for Animate.css animations*/
-$(document).ready(function() {
-  // Check if element is scrolled into view
-  function isScrolledIntoView(elem) {
-    var docViewTop = $(window).scrollTop();
-    var docViewBottom = docViewTop + $(window).height();
+// /* Loading for Animate.css animations*/
+// $(document).ready(function() {
+//   // Check if element is scrolled into view
+//   function isScrolledIntoView(elem) {
+//     var docViewTop = $(window).scrollTop();
+//     var docViewBottom = docViewTop + $(window).height();
 
-    var elemTop = $(elem).offset().top;
-    var elemBottom = elemTop + $(elem).height();
+//     var elemTop = $(elem).offset().top;
+//     var elemBottom = elemTop + $(elem).height();
 
-    return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
-  }
-  // If element is scrolled into view, fade it in
-  $(window).scroll(function() {
-    $('.scroll-animations .animated').each(function() {
-      if (isScrolledIntoView(this) === true) {
-        $(this).removeClass('hidden')
-        $(this).addClass('slideInUp');
+//     return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+//   }
+//   // If element is scrolled into view, fade it in
+//   $(window).scroll(function() {
+//     $('.scroll-animations .animated').each(function() {
+//       if (isScrolledIntoView(this) === true) {
+//         $(this).removeClass('hidden')
+//         $(this).addClass('slideInUp');
 
-      }
-    });
-  });
-}); 
+//       }
+//     });
+//   });
+// }); 
 
 /* Preloader */
 
 
-$(document).ready(function() {
-  setTimeout(function(){ 
+$(document).ready(function () {
+  setTimeout(function () {
     document.getElementById('preloader').style['display'] = 'none';
     document.getElementById('loader').style['display'] = 'none';
-  }, 2000);
+  }, 1500);
 });
 
 /* Drift */
 "use strict";
 
-!function() {
+!function () {
   var t = window.driftt = window.drift = window.driftt || [];
   if (!t.init) {
     if (t.invoked) return void (window.console && console.error && console.error("Drift snippet included twice."));
-    t.invoked = !0, t.methods = [ "identify", "config", "track", "reset", "debug", "show", "ping", "page", "hide", "off", "on" ], 
-    t.factory = function(e) {
-      return function() {
-        var n = Array.prototype.slice.call(arguments);
-        return n.unshift(e), t.push(n), t;
+    t.invoked = !0, t.methods = ["identify", "config", "track", "reset", "debug", "show", "ping", "page", "hide", "off", "on"],
+      t.factory = function (e) {
+        return function () {
+          var n = Array.prototype.slice.call(arguments);
+          return n.unshift(e), t.push(n), t;
+        };
+      }, t.methods.forEach(function (e) {
+        t[e] = t.factory(e);
+      }), t.load = function (t) {
+        var e = 3e5, n = Math.ceil(new Date() / e) * e, o = document.createElement("script");
+        o.type = "text/javascript", o.async = !0, o.crossorigin = "anonymous", o.src = "https://js.driftt.com/include/" + n + "/" + t + ".js";
+        var i = document.getElementsByTagName("script")[0];
+        i.parentNode.insertBefore(o, i);
       };
-    }, t.methods.forEach(function(e) {
-      t[e] = t.factory(e);
-    }), t.load = function(t) {
-      var e = 3e5, n = Math.ceil(new Date() / e) * e, o = document.createElement("script");
-      o.type = "text/javascript", o.async = !0, o.crossorigin = "anonymous", o.src = "https://js.driftt.com/include/" + n + "/" + t + ".js";
-      var i = document.getElementsByTagName("script")[0];
-      i.parentNode.insertBefore(o, i);
-    };
   }
 }();
 drift.SNIPPET_VERSION = '0.3.1';
